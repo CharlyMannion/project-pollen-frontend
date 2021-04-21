6f37cf0d7c436339f9813ae28d0d2192c01e1947
Madhura Bhujbal <madhura.bhujbal@and.digital>
2021-04-21 22:28:36 +0100

    Move the donation container code in seperate component

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
+import DonationContainer from "./DonationContainer";
 import '../App.css';
 
 
 function Slider() {
     const [value, onChange] = useState(0);
-    const map = {50 : "Fifty"};
     useEffect(() => {
         const element = document.querySelector(".bubble");
         if(element) {
@@ -12,35 +12,15 @@ function Slider() {
         }
     });
 
-    function handleClick(e) {
-       console.log("button clicked ");
-       onChange(25);
-    }
-
     return (
         <div className="donation-selection">
             <div className="donation-card-title">
                 <h2>Help us to make a difference.</h2>
             </div>
-            <div className="donation-container">
-                <div>
-                    <p>Donation {map[value]} is virtue</p>
-                </div>
-                <button onClick= {handleClick}>click me</button>
-                <div className="slider-parent">
-                    <input 
-                    type = 'range'
-                    min = '0'
-                    max = '100'
-                    step = '5'
-                    value = {value}
-                    onChange = {({ target: {value: radius} }) =>{
-                        onChange(radius);
-                    }}
-                    />
-                    <div className="bubble"> {value} </div>
-                </div>
-            </div>
+            <DonationContainer 
+            onChange = {onChange}
+            value = {value}
+            />
         </div>
     )
 }
