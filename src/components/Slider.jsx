63aef19269e234cc266c67b627bdb60a40423dbc
Madhura Bhujbal <madhurabhujbal@madhuras-mbp.home>
2021-04-21 15:44:47 +0100

    Create a slider component for donation range

---
+++ src/components/Slider.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import '../App.css';
+
+
+function Slider() {
+    const [value, onChange] = useState(1);
+
+    useEffect(() => {
+        const element = document.querySelector(".bubble");
+        if(element) {
+            element.style.left = `${Number(value / 4)}`;
+        }
+    });
+
+    return (
+        <div className="slider-parent">
+            <input 
+            type = 'range'
+            min = '1'
+            max = '100'
+            value = {value}
+            onChange = {({ target: {value: radius} }) =>{
+                onChange(radius);
+            }}
+            />
+            <div className="bubble"> {value} </div>
+        </div>
+    )
+}
+
+export default Slider
