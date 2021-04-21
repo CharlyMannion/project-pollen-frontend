32a406a0ed2e137a78d4cf3be6d5fd520adb293f
Madhura Bhujbal <madhurabhujbal@madhuras-mbp.home>
2021-04-21 21:46:22 +0100

    Add click button functionality and style it

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,8 +3,8 @@ import '../App.css';
 
 
 function Slider() {
-    const [value, onChange] = useState(1);
-
+    const [value, onChange] = useState(0);
+    const map = {50 : "Fifty"};
     useEffect(() => {
         const element = document.querySelector(".bubble");
         if(element) {
@@ -12,19 +12,34 @@ function Slider() {
         }
     });
 
+    function handleClick(e) {
+       console.log("button clicked ");
+       onChange(25);
+    }
+
     return (
         <div className="donation-selection">
-            <div className="slider-parent">
-                <input 
-                type = 'range'
-                min = '1'
-                max = '100'
-                value = {value}
-                onChange = {({ target: {value: radius} }) =>{
-                    onChange(radius);
-                }}
-                />
-                <div className="bubble"> {value} </div>
+            <div className="donation-card-title">
+                <h2>Help us to make a difference.</h2>
+            </div>
+            <div className="donation-container">
+                <div>
+                    <p>Donation {map[value]} is virtue</p>
+                </div>
+                <button onClick= {handleClick}>click me</button>
+                <div className="slider-parent">
+                    <input 
+                    type = 'range'
+                    min = '0'
+                    max = '100'
+                    step = '5'
+                    value = {value}
+                    onChange = {({ target: {value: radius} }) =>{
+                        onChange(radius);
+                    }}
+                    />
+                    <div className="bubble"> {value} </div>
+                </div>
             </div>
         </div>
     )
