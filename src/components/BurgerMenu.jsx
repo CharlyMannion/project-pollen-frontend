e19383c2e73a943e6c212a61e49bbdfa66d809b8
Edwin Simpson <edwinsimpson@edwins-mbp-2.home>
2021-04-22 11:25:32 +0100

    added burger menu, did some additional breakpoint styling for nav and header (not yet complete), added the quote section and styled it for mobile and desktop

---
+++ src/components/BurgerMenu.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const BurgerMenu = ()=> {
+    
+  return (
+    <div className="burger-menu">
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+  );
+};
+
+export default BurgerMenu;
