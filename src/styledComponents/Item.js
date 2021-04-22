0fba6b773da0c64b8c0f097994a66d1791ea66e1
Charly <charlottemannion100@gmail.com>
2021-04-22 10:47:23 +0100

    makes carousel look a bit better

---
+++ src/styledComponents/Item.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export default styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 250px;
+  width: 50%;
+  background-color: red;
+  color: #fff;
+  margin: 15px;
+  font-size: 2em;
+`;
