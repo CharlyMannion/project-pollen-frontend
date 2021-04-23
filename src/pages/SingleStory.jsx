ae19d9c27f13a57ed7fb8d56d5b7561aafa0219e
Charly <charlottemannion100@gmail.com>
2021-04-23 11:59:05 +0100

    makes story page look correct

--- src/pages/SingleStory.jsx
+++ src/pages/SingleStory.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import Loader from "../components/Loader";
 import ErrorDisplay from "../components/ErrorDisplay";
 import Fade from "react-reveal/Fade";
+import CharitySection from '../components/CharitySection';
 
 class SingleStory extends Component {
   state = {
@@ -13,7 +14,7 @@ class SingleStory extends Component {
   fetchStoryById = () => {
     axios.get(`https://project-pollen-backend.herokuapp.com/api/stories/${this.props.id}`)
     .then(({ data }) => {
-      console.log(data.story.quotes, "STORY DATA")
+      console.log(data.story, "STORY DATA")
         this.setState({
           story_info: data,
           isLoading: false,
@@ -42,27 +43,25 @@ class SingleStory extends Component {
     return (
       <Fade>
       <main className="single-page-story">
-        <br/>
-        <body>
-          <h1>{story_info.name}'s Story</h1>
-          <br/>
-          <h2>{story_info.story.title}</h2>
-        </body>
-        <figure>
-          <img
-          className="avatar"
-          src={story_info.image}
-          alt="missing story image..."
-          ></img>
-          <figcaption class="caption">{story_info.story.title}</figcaption>
-         </figure>
-         <section/>
-          <p>{story_info.story.paragraphs[0]}</p>
-          <h3>"{story_info.story.quotes[1]}"</h3>
-          <p>{story_info.story.paragraphs[1]}</p>
-          <p>{story_info.story.paragraphs[2]}</p>
-         <section/>
+        <div className="story-column-one">
+          <h1>{story_info.story.title}</h1><br/>
+          <p1>{story_info.story.quotes[0]}</p1><br/><br/>
+          <img className="avatar" src={story_info.image} alt="missing story image..."></img><br/>
+          <p3>{story_info.story.paragraphs[0]}</p3><br/>
+          <p3>{story_info.story.paragraphs[1]}</p3><br/><br/>
+          <p4>{story_info.story.quotes[1]}</p4><br/><br/>
+          <p3>{story_info.story.paragraphs[2]}</p3><br/>
+          <p3>{story_info.story.paragraphs[3]}</p3><br/><br/>
+          <p4>{story_info.story.quotes[2]}</p4><br/><br/>
+          <p3>{story_info.story.paragraphs[4]}</p3><br/><br/>
+          <p2>Manchester Homeless Charity needs your help. To help others like {story_info.name} facing homelessness, donate today. As little as £5 will provide a meal and a bed to someone in need.</p2><br/>
+        </div>
+        <div className="story-column-two">
+          <img className="avatar" src={story_info.image} alt="missing story image..."></img><br/><br/>
+          <p2>Manchester Homeless Charity needs your help. To help others like {story_info.name} facing homelessness, donate today. As little as £5 will provide a meal and a bed to someone in need.</p2><br/>
+        </div>
       </main>
+      <CharitySection />
       </Fade>
     );
   }
